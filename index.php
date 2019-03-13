eee3881751d810b0c96f3b20d9e0e0c38449f08a
cetecom <cetecom@LCV20421>
2019-03-13 20:47:44 -0300

    subida de ejemplo 3 css 3 div

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link href="estilos.css" rel="stylesheet" type="text/css"/>
+        <title>Ejercicio posición elementos</title>
+    </head>
+    <body>
+        
+        <div id="menu">menu</div>
+        <div id="head">head</div>
+        <div id="contenedor">contenedor</div>
+    </body>
+</html>
